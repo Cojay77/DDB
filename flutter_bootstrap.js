dc635ff9517650d4439d4065a51c82a8bb294cae
Cojay77 <Cojay77@users.noreply.github.com>
2025-04-15 11:46:01 +0000

    deploy: 2ef451f8fe77a2ac6c40b6ea7479840508ab72eb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "173183590"
+    serviceWorkerVersion: "2693729203"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,15 +29,15 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "assets/AssetManifest.json": "0fe6fce609cd7c2227aac8eb785bdc65",
 "version.json": "a72ac8a82d236281034acbb5f90959c1",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"main.dart.js": "d0230fe33517319d12ae943f63275246",
-"firebase-messaging-sw.js": "ac02e7bed81b164a3054d9c2b1adb6a6",
+"main.dart.js": "41dbae27bb0debaa206728d1378721c9",
+"firebase-messaging-sw.js": "338d2b268e8a4ae9ac5cfd186e4d991f",
 "icons/Icon-maskable-512.png": "894cc400c389b6868c30d4d7b3978859",
 "icons/Icon-192.png": "894cc400c389b6868c30d4d7b3978859",
 "icons/Icon-512.png": "894cc400c389b6868c30d4d7b3978859",
 "icons/Icon-maskable-192.png": "894cc400c389b6868c30d4d7b3978859",
 "manifest.json": "834c6725c181760cca8bd9998a841c4b",
 "favicon.png": "894cc400c389b6868c30d4d7b3978859",
-"flutter_bootstrap.js": "592800e03d452ddb871105d4fd2adf05"};
+"flutter_bootstrap.js": "9d1c2e3a54a988ca46b44fee680e1f20"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
