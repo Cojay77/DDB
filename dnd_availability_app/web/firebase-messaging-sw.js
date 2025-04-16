1f7a8ac5e1eb6f25117030857b3f0fa5c2caab98
Cojay <corentin.jayer@gmail.com>
2025-04-16 15:59:58 +0200

    Test messaging copy

--- dnd_availability_app/web/firebase-messaging-sw.js
+++ dnd_availability_app/web/firebase-messaging-sw.js
@@ -21,11 +21,3 @@ messaging.onBackgroundMessage((payload) => {
     icon: '/DDB/icons/Icon-192.png'
   });
 });
-
-// Fichier factice pour éviter erreur 404
-// Redirige vers le vrai SW s'il est chargé en doublon
-
-self.addEventListener('install', () => {
-  console.log("✅ Faux firebase-messaging-sw.js à la racine chargé.");
-});
-
