352975dfb7df552c37986a93f71235e5de01ad5d
Cojay77 <corentin.jayer@gmail.com>
2025-04-17 10:26:34 +0200

    Removed unused

--- firebase-messaging-sw-copy.js
+++
@@ -1,7 +0,0 @@
-// Fichier factice pour éviter erreur 404
-// Redirige vers le vrai SW s'il est chargé en doublon
-
-self.addEventListener('install', () => {
-  console.log("✅ Faux firebase-messaging-sw.js à la racine chargé.");
-});
-
