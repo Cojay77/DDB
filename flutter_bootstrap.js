f4c1cf205fa9dcd68f0eeeca291bb50cae020af4
Cojay77 <Cojay77@users.noreply.github.com>
2025-04-17 11:27:41 +0000

    deploy: 855c14e2cd93e30756d18e79d2949bd5142b1b75

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4162364967"
+    serviceWorkerVersion: "1123365849"
   }
 });
